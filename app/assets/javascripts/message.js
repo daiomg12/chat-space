50a0d1a86baa2cd8462e8fb096c2710973d42189
daiomg12 <dai.ohno.1212@gmail.com>
2020-05-27 20:28:41 +0900

    非同期通信実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,45 @@
 $(function(){
-  $('#new_message').on('submit', function(e){
+     function buildHTML(message){
+      if ( message.image ) {
+        var html =
+         `<div class="message-box">
+            <div class="message-box__sender">
+              <div class="message-box-sender__name">
+                ${message.user_name}
+              </div>
+              <div class="message-box__sender__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message-box__content">
+              <p class="message-box__content__detail">
+                ${message.content}
+              </p>
+              <img src=${message.image} >
+            </div>
+          </div>`
+        return html;
+      } else {
+        var html =
+         `<div class="message-box">
+            <div class="message-box__sender">
+              <div class="message-box-sender__name">
+                ${message.user_name}
+              </div>
+              <div class="message-box__sender__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message-box__content">
+              <p class="message-box__content__detail">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
+$('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -11,5 +51,15 @@ $(function(){
       processData: false,
       contentType: false
     })
-  })
-})
+     .done(function(data){
+       var html = buildHTML(data);
+       $('.main-chat__messages').append(html);
+       $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
+       $('form')[0].reset();
+     })
+      .fail(function(){
+        alert("メッセージ送信に失敗しました");
+      });
+      return false;
+    });
+});
