57a71f9a3ea179cfaa11ce559d8e8ee01e3c4761
daiomg12 <dai.ohno.1212@gmail.com>
2020-05-28 18:15:57 +0900

    チャット画面自動更新機能追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
      function buildHTML(message){
       if ( message.image ) {
         var html =
-         `<div class="message-box">
+         `<div class="message-box" data-message-id=${message.id}>
             <div class="message-box__sender">
               <div class="message-box-sender__name">
                 ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
         return html;
       } else {
         var html =
-         `<div class="message-box">
+         `<div class="message-box" data-message-id=${message.id}>
             <div class="message-box__sender">
               <div class="message-box-sender__name">
                 ${message.user_name}
@@ -44,9 +44,9 @@ $('#new_message').on('submit', function(e){
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: 'POST',  //同期通信でいう『HTTPメソッド』
-      data: formData,  
+      url: url,
+      type: 'POST', 
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
@@ -57,9 +57,34 @@ $('#new_message').on('submit', function(e){
        $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
        $('form')[0].reset();
      })
-      .fail(function(){
-        alert("メッセージ送信に失敗しました");
+     .fail(function(){
+          alert("メッセージ送信に失敗しました");
       });
       return false;
     });
+  var reloadMessages = function() {
+    var last_message_id = $('.message-box:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__messages').append(insertHTML);
+        $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
